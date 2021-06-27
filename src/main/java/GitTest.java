6ec559333072f7bd6dae6ffe09ea09ea66548a10
Jiangjiajun <Ohh5015@163.com>
2021-06-27 23:09:59 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: jj
+ * @Data: 2021/06/23:07
+ * @Discription:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+}
