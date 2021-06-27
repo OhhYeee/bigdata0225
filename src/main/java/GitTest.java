6afa0ff1f1e9940ac502eb38e6bb327fe1bfa5d4
Jiangjiajun <Ohh5015@163.com>
2021-06-27 23:23:40 +0800

    master commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,12 +1,11 @@
 /**
  * @Author: jj
- * @Data: 2021/06/23:07
+ * @Data: 2021/06/23:22
  * @Discription:
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("git");
-        System.out.println("hello");
+        System.out.println("Git");
         System.out.println("master");
     }
 }
